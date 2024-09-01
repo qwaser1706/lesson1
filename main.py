69b4512c9163865ba65364fcaf36421ad8bc1207
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-09-01 03:02:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from operator import truth
+#1st program
+print(9**0.5*5)
+
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == 2 * (2 + 2))
+
+
+#4th program
+a = '123.456'
+a = float(a)
+print(int(a * 10) % int(a))
+
