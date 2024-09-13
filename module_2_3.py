22e2ece2093e118d4a56d5e55e319e81097732b9
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-09-14 01:54:19 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+int(a)
+b = 1
+while b >= 0 or a != (len(my_list)):
+    if b >= 0 or a != (len(my_list)):
+        b = (my_list[a])
+        a += 1
+    if b == 0:
+        continue
+    if b > 0 :
+        print(b)
+    else:
+        break
