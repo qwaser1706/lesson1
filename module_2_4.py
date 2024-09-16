fa975277b208fa14f3818f5f0f93eaefe33249b3
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-09-16 23:23:56 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,21 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+Primes = []
+Not_Primes = []
+
+for i in numbers:
+    if i == 1:
+        continue
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        Primes.append(i)
+    else:
+        Not_Primes.append(i)
+
+
+
+print(f'Primes: {Primes}')
+print(f'Not_Primes: {Not_Primes}')
