e70fd57f8a647b2028a3d5f9cd4a82203b3b08be
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-09-19 23:46:03 +0300

    module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -7,8 +7,6 @@ def get_matrix(n, m, value):
             a.append(value)
     return matrix
 
-
-get_matrix
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
