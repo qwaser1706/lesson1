ba07a2c494534d5c1809195dbc83fb5532f4dd7a
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-10-02 23:18:20 +0300

    module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,11 +1,13 @@
 def single_root_words(root_word, *other_wods):
     single_root_words = []
-    for w in other_wods:
-        if root_word.lower() in w.lower():
-            single_root_words.append(w)
-
+    for i in other_wods:
+        if root_word.lower() in i.lower():
+            single_root_words.append(i)
+    for y in other_wods:
+        if y.lower() in root_word.lower():
+            single_root_words.append(y)
     return single_root_words
 
 
 print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
-print(single_root_words( 'Able','Disablement', 'Mable', 'Disable', 'Bagel'))
+print(single_root_words( 'Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
