e6a6106f6c4faf72f28f6debc40be76ccbcaa2c1
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-10-26 15:42:59 +0300

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,11 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
+    str_number1 = str(number)
+    str_number = str_number1 + "1"
     first = int(str_number[0])
     if len(str_number) > 1:
         first *= get_multiplied_digits(int(str_number[1:]))
     return first
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(402030)
 print(result)
