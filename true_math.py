c2e742ce9ce1d50d6af046ab3fdb448787969eb4
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-10-27 19:37:53 +0300

    true_math.py

--- true_math.py
+++ true_math.py
@@ -1,5 +1,10 @@
-from math import *
+import math
+
 
 def true_divide(first, second):
-    q = first / second
+    if second == 0:
+        q = math.inf
+    else:
+        q = first / second
     return q
+
