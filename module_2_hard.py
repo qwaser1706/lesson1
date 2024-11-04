ad9143706dd2279e65f7ca722676bb7ec7c9b9c3
qwaser1706 <93528718+qwaser1706@users.noreply.github.com>
2024-11-05 01:52:33 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,25 +1,24 @@
 import random
+
+
 def one_():
-    a = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    a = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     one = random.choice(a)
     return one
+
+
 one = one_()
-while 1<2 :
-    import random
-    def two1():
-        b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-        b.remove(one)
-        two1 = random.choice(b)
-        b.remove(two1)
-        two2 = random.choice(b)
-
-        return two1, two2
-    two1, two2 = two1()
-
-    if one % (two1 +two2) == int(0):
-        break
-    else:
-        continue
-print(f" {one} - {two1} + {two2}")
+one_()
 
 
+def two_(one):
+    two = ''
+    for i in range(1, one):
+        for j in range(i + 1, one + 1):
+            if one % (i + j) == 0:
+                two += str(i) + str(j)
+    return two
+
+
+two = two_(one)
+print(f'{one}-{two}')
